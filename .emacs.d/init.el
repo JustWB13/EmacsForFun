b78c1fc5fab2bd753fc457c19d6e7a3b5a84d1f3
JustWB <justwb13@gmail.com>
2023-04-20 15:03:56 +0800

    add company-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,8 @@
       '(
 		(?\" . ?\")  ;; 添加双引号补齐
 		(?\{ . ?\})  ;; 添加大括号补齐
-		(?\' . ?\'))) ;; 添加单引号补齐
+		(?\' . ?\')
+        (?\< . ?\>))) ;; 添加单引号补齐
 (setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
                          ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
@@ -25,10 +26,21 @@
  '(custom-enabled-themes '(monokai))
  '(custom-safe-themes
    '("95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692" default))
- '(package-selected-packages '(monokai-theme)))
+ '(package-selected-packages '(company monokai-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(use-package company
+  :ensure t
+  :init (global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
+  (setq company-tooltip-align-annotations t)
+  (setq company-idle-delay 0.0)
+  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
+  (setq company-selection-wrap-around t)
+  (setq company-transformers '(company-sort-by-occurrence))) ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
